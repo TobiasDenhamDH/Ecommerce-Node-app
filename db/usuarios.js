a08dd84ff3ab14fa1e676077acdce70e3295afac
TobiasDenhamDH <tdenham@udesa.edu.ar>
2022-04-11 11:29:48 -0300

    completando modulo de usuarios

--- db/usuarios.js
+++ db/usuarios.js
@@ -1,10 +1,35 @@
-let usuarios = {
-  nombre: "Felipe Perez",
+let usuarios = [
+    
+{ nombre: "Felipe Perez",
   imagen: "/images/users/default-image.png",
   productos: "10 productos",
   seguidores: "1345 seguidores",
   comentarios:"660 comentarios"
- 
+},
+{ nombre: "Sebastian Chiterer",
+  imagen: "/images/users/default-image.png",
+  productos: "5 productos",
+  seguidores: "708 seguidores",
+  comentarios:"56 comentarios"
+},
+{ nombre: "Ana Teperman",
+  imagen: "/images/users/default-image.png",
+  productos: "304 productos",
+  seguidores: "13 seguidores",
+  comentarios:"2 comentarios"
+},
+{ nombre: "Martin Lotz",
+  imagen: "/images/users/default-image.png",
+  productos: "34 productos",
+  seguidores: "35 seguidores",
+  comentarios:"354 comentarios"
+},
+{ nombre: "Tobias Denham",
+  imagen: "/images/users/default-image.png",
+  productos: "3 productos",
+  seguidores: "134 seguidores",
+  comentarios:"563 comentarios"
 }
+]
 
 module.exports=usuarios;
