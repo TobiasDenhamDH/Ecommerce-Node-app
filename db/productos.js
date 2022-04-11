06ccfd3addd9a8ac05643d08578bfdc618ad75cb
Ana Teperman <tepermana@udesa.edu.ar>
2022-04-11 11:09:46 -0300

    productos database

--- db/productos.js
+++ db/productos.js
@@ -1,7 +1,77 @@
+const src = require("debug");
+
 let productos = [
     {
-
+        foto :"/images/products/air-force.jpg",
+        nombre: "Zapatillas Nike Air Force 1" ,
+        color : "Color: Blanco" ,
+        talle : "Talle: 41/42" ,
+        comentario:" Comentarios : 12" ,
     },
+    {
+        foto : "/images/products/air-force-1-botas-wWrQ12.jpg" ,
+        nombre: "Zapatillas Nike Air Force 1 Boots" ,
+        color : "Color: Blanco" ,
+        talle : "Talle: 39/40" ,
+        comentario:" Comentarios : 120" ,
+    },
+    {
+        foto : "/images/products/air-max-270.jpg",
+        nombre: "Zapatillas Nike Air Max 270" ,
+        color : "Color: Rosa" ,
+        talle : "Talle: 37/38" ,
+        comentario:" Comentarios : 520" ,
+    },
+    {
+        foto : "/images/products/converse-PRINT.jpg",
+        nombre: "Zapatillas Converse Plataforma " ,
+        color : "Color: Animal Print" ,
+        talle : "39/40" ,
+        comentario:" Comentarios : 115" ,
+    },
+    {
+        foto : "/images/products/forum-bold.jpg",
+        nombre: "Zapatillas Adidas Forum Bold" ,
+        color : "Color: Blanco y azul" ,
+        talle : "41/42" ,
+        comentario:" Comentarios : 13" ,
+    },
+    {
+        foto : "/images/products/jordan-air-1.jpg",
+        nombre: "Zapatillas Nike Air Jordan 1" ,
+        color : "Color: Negro" ,
+        talle : "Talle: 41/42" ,
+        comentario:" Comentarios : 48" ,
+    },
+    {
+        foto : "/images/products/jordan-11.jpg",
+        nombre: "Zapatillas Nike Air Jordan 11" ,
+        color : "Color: Negro y rojo" ,
+        talle : "Talle: 43/44" ,
+        comentario:" Comentarios : 132" ,
+    },
+    {
+        foto : "/images/products/puma-rs.jpg",
+        nombre: "Zapatillas Puma RS" ,
+        color : "Color: Blanco y rojo" ,
+        talle : "Talle: 41/42" ,
+        comentario:" Comentarios : 1212" ,
+    },
+    {
+        foto : "/images/products/superstar.jpg",
+        nombre: "Zapatillas Adidas Superstar" ,
+        color : "Color: Blanco y lila" ,
+        talle : "Talle: 35/36" ,
+        comentario:" Comentarios : 112" ,
+    },
+    {
+        foto :  "/images/products/vans-old-skool.jpg",
+        nombre: "Zapatillas Vans Old Skool" ,
+        color : "Color: Negro" ,
+        talle : "Talle: 41/42" ,
+        comentario:" Comentarios : 512" ,
+    },
+
 ]
 
 module.exports(productos);
