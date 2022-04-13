5be80c3a0a714a7220437977475a955974532f20
Sebastian Chiterer <sebachiterer@gmail.com>
2022-04-13 10:02:10 -0300

    Metiendole a comentarios

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,10 @@
+const req = require("express/lib/request");
 let usuarios = require("../db/usuarios");
+let comentarios = require('../db/comentarios')
 
 let productController = {
     index: function(req,res) {
-      return res.render('product')
+      return res.render('product', {comentarios: comentarios.lista})
     },
     add: function(req,res) {
         return res.render('product-add', {usuarios: usuarios[0].nombre})
--- db/comentarios.js
+++ db/comentarios.js
@@ -1,8 +1,36 @@
-let comentarios = [
+let comentarios = {
+    lista: [
     {
-        
-    }
-
-]
+       usuario: 'Carlos Izquierdoz',
+       texto:'Este producto me cambió la vida, desde que me las puse tengo mas levamte que Brad Pitt.',
+       foto:'/images/users/izquierdoz.jpg'
+    },
+    {
+        usuario: 'Leonardo DiCaprio',
+        texto:'These sneakers are incredible. I wore them to the last oscars and was everyone',
+        foto:'/images/users/leo.jpg'
+     },
+     {
+        usuario: 'Cristiano Ronaldo',
+        texto:'Estas zapatilliñas son las mejores que tengo actualmenchi. El bicho las recomiendo completamente, son extremadamente cómodas. Siuuuuuuuuuuuuuuuuu',
+        foto:'/images/users/bicho.jpg'
+     },
+     {
+        usuario: 'floyd mayweather',
+        texto:'Estas zapatillas son horribles, son muy incomodas. Son de mi talle pero no se terminan de adaptar a mis pies. Las voy a devolver!',
+        foto:'/images/users/floyd.jpg'
+     },
+     {
+        usuario: 'johnny depp',
+        texto:'Hello, what is the price of these shoes? I am thinking of buying them. Please reply me as soon as possible.',
+        foto:'/images/users/deep.jpg'
+     },
+     {
+        usuario: 'Conor McGregor',
+        texto:'jajajaja estas zapatillas son una locura. Floyd ya te vi comentando acá, cuando quieras hacemos la revancha.',
+        foto:'/images/users/mcgregor.jpg'
+     }
+    ]
+}
 
 module.exports=comentarios;
