3796a79474b7f98a25451ec97c270cb792e6a9a1
Ana Teperman <tepermana@udesa.edu.ar>
2022-04-14 17:36:46 -0300

    main con productos y más comentados

--- db/productos.js
+++ db/productos.js
@@ -2,8 +2,9 @@ const src = require("debug");
 
 let productos = [
     {
-        foto :"/images/products/air-force.jpg",
+        foto : '/images/products/air-force.jpg ' ,
         nombre: "Zapatillas Nike Air Force 1" ,
+        marca: "Marca: Nike",
         color : "Color: Blanco" ,
         talle : "Talle: 41/42" ,
         comentario:" Comentarios : 12" ,
@@ -11,6 +12,7 @@ let productos = [
     {
         foto : "/images/products/air-force-1-botas-wWrQ12.jpg" ,
         nombre: "Zapatillas Nike Air Force 1 Boots" ,
+        marca: "Marca: Nike",
         color : "Color: Blanco" ,
         talle : "Talle: 39/40" ,
         comentario:" Comentarios : 120" ,
@@ -18,6 +20,7 @@ let productos = [
     {
         foto : "/images/products/air-max-270.jpg",
         nombre: "Zapatillas Nike Air Max 270" ,
+        marca : "Marca: Nike",
         color : "Color: Rosa" ,
         talle : "Talle: 37/38" ,
         comentario:" Comentarios : 520" ,
@@ -25,6 +28,7 @@ let productos = [
     {
         foto : "/images/products/converse-PRINT.jpg",
         nombre: "Zapatillas Converse Plataforma " ,
+        marca: "Marca: Converse",
         color : "Color: Animal Print" ,
         talle : "39/40" ,
         comentario:" Comentarios : 115" ,
@@ -32,6 +36,7 @@ let productos = [
     {
         foto : "/images/products/forum-bold.jpg",
         nombre: "Zapatillas Adidas Forum Bold" ,
+        marca : "Marca: Adidas",
         color : "Color: Blanco y azul" ,
         talle : "41/42" ,
         comentario:" Comentarios : 13" ,
@@ -39,6 +44,7 @@ let productos = [
     {
         foto : "/images/products/jordan-air-1.jpg",
         nombre: "Zapatillas Nike Air Jordan 1" ,
+        marca: "Marca: Nike",
         color : "Color: Negro" ,
         talle : "Talle: 41/42" ,
         comentario:" Comentarios : 48" ,
@@ -46,13 +52,15 @@ let productos = [
     {
         foto : "/images/products/jordan-11.jpg",
         nombre: "Zapatillas Nike Air Jordan 11" ,
+        marca: "Marca: Nike",
         color : "Color: Negro y rojo" ,
         talle : "Talle: 43/44" ,
         comentario:" Comentarios : 132" ,
     },
     {
         foto : "/images/products/puma-rs.jpg",
-        nombre: "Zapatillas Puma RS" ,
+        nombre: "Zapatillas Puma RS- Fast" ,
+        marca: "Marca: Puma",
         color : "Color: Blanco y rojo" ,
         talle : "Talle: 41/42" ,
         comentario:" Comentarios : 1212" ,
@@ -60,18 +68,35 @@ let productos = [
     {
         foto : "/images/products/superstar.jpg",
         nombre: "Zapatillas Adidas Superstar" ,
+        marca: "Marca: Adidas",
         color : "Color: Blanco y lila" ,
         talle : "Talle: 35/36" ,
         comentario:" Comentarios : 112" ,
     },
     {
-        foto :  "/images/products/vans-old-skool.jpg",
+        foto :  "/images/products/vans.jpg",
         nombre: "Zapatillas Vans Old Skool" ,
+        marca: "Marca: Vans",
         color : "Color: Negro" ,
         talle : "Talle: 41/42" ,
         comentario:" Comentarios : 512" ,
     },
-
+    {
+        foto :  "/images/products/taylor-all-move.jpg",
+        nombre: "Zapatillas Chuck Taylor All Star Move" ,
+        marca : "Marca: Converse",
+        color : "Color: Blanco" ,
+        talle : "Talle: 39/40" ,
+        comentario:" Comentarios : 712" ,
+    },
+    {
+        foto : "/images/products/forum-mid.jpg",
+        nombre: "Zapatillas Adidas Forum Mid" ,
+        marca : "Marca: Adidas",
+        color : "Color: Blanco" ,
+        talle : "41/42" ,
+        comentario:" Comentarios : 130" ,
+    },
 ]
 
 module.exports = productos;
