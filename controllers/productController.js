e454ba0b6b41b00f8ea9d8add2eb9e69d57c7bde
Ana Teperman <tepermana@udesa.edu.ar>
2022-06-13 11:09:36 -0300

    Intentandp agregar productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const req = require("express/lib/request");
 let usuarios = require("../db/usuarios");
-let comentarios = require('../db/comentarios');
+let comentarios = require('../db/comentarios'); 
 
 let db = require ("../database/models");
 let op = db.Sequelize.Op;
@@ -16,10 +16,34 @@ let productController = {
     },
 
     store: function(req,res) {
-      return res.render('product-add', {usuarios: usuarios})
-  }
+      let errors = {}
+      if (req.body.name == ""){
+        errors.message = "El nombre es obligatorio"
+        res.locals.errors = errors;
+        return res.render('product-add')
+      } else if (req.file.mimetype !== 'image/png' && req.file.mimetype !== 'image/jpg' && req.file.mimetype !== 'image/jpeg') {
+        errors.message = "El archivo debe ser una imagen"
+        res.locals.errors = errors;
+        return res.render('product-add')
+      } 
+      else{
+        let product = {
+          users_id: 1,
+          image: req.file.filename,
+          name: req.body.name,
+          description: req.body.description,
+        }
+        db.Product.create (product)
+        .then( productGuardado=> {
+          return res.redirect('/')
+      })
+      .catch(error => console.log(error))
+    }
+      
 
-  }
+      
+
+}}
 
 
-  module.exports = productController;
+  module.exports = productController
