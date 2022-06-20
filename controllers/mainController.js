8446bdd073098ac75bdc0f6c6ea3d73850246d1a
Ana Teperman <tepermana@udesa.edu.ar>
2022-06-20 13:27:26 -0300

    main page

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -6,7 +6,22 @@ let op = db.Sequelize.Op;
 
 let mainController = {
   index: function(req,res) {
-    return res.render('main', {productos:productos})
+    db.Product.findAll({
+      include: [  
+        { association: 'comments' }, 
+       /* Relacion de comentario con producto */
+                                
+        {association: 'users'}
+        /*Relación de usuarios con productos*/
+      ],
+      order:  [ ["created_at", "DESC" ] ]
+    })
+    .then ((data) => {
+      return res.render ("main", {data: data})
+    })
+    .catch((err)=>{
+      console.log(err)
+  })
   }
   
 }
